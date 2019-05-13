440d8e0e5794fb6fa076fb6aeb3313dbef2e55c0
Gia Bao <gbao.dev@gmail.com>
2019-05-13 21:19:14 +0700

    Create Skill

---
+++ js/jquery.js
@@ -0,0 +1,18 @@
+﻿ $(function(){
+
+ 	$(".btn2page,.about").click(function(){
+ 		$('body,html').animate({scrollTop:$('#blockabout').offset().top}, 800);
+ 		return false;
+ 	})
+
+	$(".skill").click(function(){
+ 		$('body,html').animate({scrollTop:$('#blockskill').offset().top}, 800);
+ 		return false;
+ 	})
+
+	$(".contact").click(function(){
+ 		$('body,html').animate({scrollTop:$('#blockcontact').offset().top}, 800);
+ 		return false;
+ 	})	
+})  
+ 
